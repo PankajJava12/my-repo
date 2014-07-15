d058659dc33f0bc227e8d594c66ac0f60fdc0804
PankajJava12 <pankaj.java12@gmail.com>
2014-07-15 19:21:21 +0530

    Yeah

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
-// Load the http module to create an http server.
-var http = require('http');
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Pankaj Kumar Dewangan, This is live Server of NodeJs\n");
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
